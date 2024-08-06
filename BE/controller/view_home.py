05dcc8a0d9e7c976f47c36838d4ff7dae0aeec90
Nguyen Thanh Hy <job.bkth@gmail.com>
2024-08-06 23:17:47 +0700

    fix bug .ddl file

--- BE/controller/view_home.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, Blueprint
-
-view_home_bp = Blueprint('home', __name__, template_folder=...)
-
-@view_home_bp.route('/home')
-def get_home_page():
-    return render_template('home.html')
