e09fc666f13cab0daa519df766589fe9816f2cee
Nguyen Thanh Hy <job.bkth@gmail.com>
2024-08-21 20:10:46 +0700

    add code for frames extractin

--- BE/milvus/utils/__init__.py
+++ BE/milvus/utils/__init__.py
@@ -1,16 +1,9 @@
 from PIL import Image
 import torch
-from transformers import CLIPProcessor, CLIPModel
-
-class CLIPManager:
-    def __init__(self):
-        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 class FirstCollectionConstant:
     def __init__(self, collection_name='image_collection', metric_type='L2') -> None:
         self.collection_name = collection_name
         self.metric_type = metric_type
 
-clip_manager = CLIPManager()
 first_collection_constant = FirstCollectionConstant()
