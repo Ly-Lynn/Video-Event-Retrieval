4ea2c4a896cd732e40c412053105937faab9e000
Ly-Lynn <22520766@gmail.com>
2024-08-29 21:31:09 +0700

    add BE frame (2)

--- BE/filters/pre_processing.py
+++ BE/filters/pre_processing.py
@@ -1,27 +1,56 @@
 '''
     Xử lí offline dữ liệu: gồm xử lí OCR, ASR, OD và thêm dữ liệu vào Elastic Search
 '''
-from filters.ocr import getOCR
-from filters.asr import getASR
-from filters.od import getOD
-from filters.elasticSearch import add_to_elastic
+from elasticsearch import Elasticsearch, helpers
+from configs import configs
+import json
+import os
 
-# Thay đổi tham số phù hợp
-ocr_res = getOCR()
-asr_res = getASR()
-od_res = getOD()
+def load_json(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
 
-def createElastic (frameIDs):
+ocr = load_json(configs['OCR_DATA'])
+asr = load_json(configs['ASR_DATA'])
+od = load_json(configs['OD_DATA'])
+
+def add_to_elastic(data_list):
+    client = Elasticsearch(cloud_id=configs['CLOUD_ID'], api_key=configs['API_KEY'])
+    actions = [
+        {
+            "_index": "frames",
+            "_id": frame['id'],
+            "_source": {
+                "ocr_res": frame['ocr_res'],
+                "asr_res": frame['asr_res'],
+                'od_res': frame['od_res']
+            }
+        }
+        for frame in data_list
+    ]
+    helpers.bulk(client, actions)
+
+def createElastic():
     '''
     Input: list[frameIDs]
     Return: none
     '''
+    llist = os.listdir(configs['FRAMES_PATH'])
+    
+    frameIDs = [os.path.splitext(each)[0] for each in llist]
     data_list = []
     for frameid in frameIDs:
-        data_list.append({
-            'id': frameid,
-            'ocr_res': ocr_res[frameid],
-            'asr_res': asr_res[frameid],
-            'od_res': od_res[frameid]
-        })
+        if frameid in ocr and frameid in asr and frameid in od:
+            data_list.append({
+                'id': frameid,
+                'ocr_res': ocr[frameid],
+                'asr_res': asr[frameid],
+                'od_res': od[frameid]
+            })
+        else:
+            print(f"Warning: Data for frameid '{frameid}' not found in one or more datasets.")
+    
     add_to_elastic(data_list)
+
+if __name__ == '__main__':
+    createElastic()
