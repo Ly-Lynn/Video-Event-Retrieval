695683ca8bfa1342a77e72877a3fec764f934817
Ly-Lynn <linhlnt.work@gmail.com>
2024-09-18 23:52:31 +0700

    final stage search

--- BE/controller/api.py
+++ BE/controller/api.py
@@ -1,7 +1,12 @@
 from flask import request, Blueprint, request, jsonify
 import json
+import pandas as pd
+import os
+import sys
+from BE.configs.configs_info import configs as cfg
 # import processing
 # from milvus.utils.function import search_query
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 api_bp = Blueprint('api', __name__)
 
@@ -42,8 +47,9 @@ def get_stages ():
     '''
     data = request.json
     print(data)
-    # results = processing(data)
-    results = []
+    #--------------TEST---------------
+    with open(r'D:\codePJ\AIC\FE\aic\BE\test\test_stage.json', 'r') as file:
+        results = json.load(file)
     return jsonify(results)
 
 
@@ -55,7 +61,29 @@ def get_result_by_text():
     else:
         return {'error': 'No text provided'}, 400
     
-
+@api_bp.route('/get-submission', methods=['POST'])
+def get_submission():
+    """
+    data nhận từ FE có dạng:
+    {
+        id: string,
+        vid: string,
+        score: float,
+        query: string,
+        ocr: {query: string, weight: int},
+        asr: {query: string, weight: int},
+        od: {results:các list({object:string, coordinates:list(4)},
+            weight: int}
+    }
+    """
+    receive = request.json
+    output_file = cfg['OUTPUT_PATH'] + receive['file'] + '.csv'
+    data = receive['data']
+    vid = [dt['vid'] for dt in data]
+    vid_id = [int(dt['id']) for dt in data]
+    df = pd.DataFrame.from_dict({"video_name" : vid, "index" : vid_id})
+    df.to_csv(output_file, header=False, index=False)
+    return jsonify({'status': 'success'})
 
 
 '''
